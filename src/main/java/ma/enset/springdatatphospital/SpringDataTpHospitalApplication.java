a0c4be75fe71c363a941f628edbc3f694034b098
ichou <ichou.bahba@gmail.com>
2023-04-03 08:03:35 +0000

    first commit

---
+++ src/main/java/ma/enset/springdatatphospital/SpringDataTpHospitalApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.springdatatphospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataTpHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataTpHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/springdatatphospital/SpringDataTpHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.springdatatphospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataTpHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
